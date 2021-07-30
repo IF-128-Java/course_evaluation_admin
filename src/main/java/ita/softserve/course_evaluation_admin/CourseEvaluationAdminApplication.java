1bb7311ae57662968a91d7cc663a2742c50d0ff8
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-30 16:43:27 +0300

    initial commit

---
+++ src/main/java/ita/softserve/course_evaluation_admin/CourseEvaluationAdminApplication.java
@@ -0,0 +1,13 @@
+package ita.softserve.course_evaluation_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEvaluationAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseEvaluationAdminApplication.class, args);
+    }
+
+}
