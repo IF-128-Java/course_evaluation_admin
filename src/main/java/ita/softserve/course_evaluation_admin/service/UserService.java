062c6df04f4e97d3714c19351bdd1897c188e0b3
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-07-31 14:58:45 +0300

    added UserService

---
+++ src/main/java/ita/softserve/course_evaluation_admin/service/UserService.java
@@ -0,0 +1,17 @@
+package ita.softserve.course_evaluation_admin.service;
+
+import ita.softserve.course_evaluation_admin.dto.user.UserDto;
+import ita.softserve.course_evaluation_admin.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserDto> findAll();
+
+    User findById(long id);
+
+    User update(UserDto dto);
+
+
+}
---
+++ src/main/java/ita/softserve/course_evaluation_admin/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package ita.softserve.course_evaluation_admin.service.impl;
+
+import ita.softserve.course_evaluation_admin.dto.user.UserDto;
+import ita.softserve.course_evaluation_admin.entity.User;
+import ita.softserve.course_evaluation_admin.exception.exceptions.WrongEmailException;
+import ita.softserve.course_evaluation_admin.exception.exceptions.WrongIdException;
+import ita.softserve.course_evaluation_admin.repository.UserRepository;
+import ita.softserve.course_evaluation_admin.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static ita.softserve.course_evaluation_admin.dto.user.UserDtoMapper.fromDto;
+import static ita.softserve.course_evaluation_admin.dto.user.UserDtoMapper.toDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserDto> findAll() {
+        return toDto(userRepository.findAll());
+    }
+
+    @Override
+    public User findById(long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new WrongIdException("The user does not exist by this id: " + id));
+    }
+
+    @Override
+    public User update(UserDto dto) {
+        Long id = dto.getId();
+        User user = findById(id);
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setRoles(dto.getRoles());
+        return userRepository.save(user);
+    }
+
+
+}
