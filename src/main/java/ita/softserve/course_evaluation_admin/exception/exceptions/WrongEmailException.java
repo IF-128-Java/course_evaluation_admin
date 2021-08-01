3c443fcd87059b67ce5decba8eb8fb7a764ef2c6
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-01 11:48:35 +0300

    changed UserService

--- src/main/java/ita/softserve/course_evaluation_admin/exception/exceptions/WrongEmailException.java
+++
@@ -1,7 +0,0 @@
-package ita.softserve.course_evaluation_admin.exception.exceptions;
-
-public class WrongEmailException extends RuntimeException {
-    public WrongEmailException(String message) {
-        super(message);
-    }
-}
