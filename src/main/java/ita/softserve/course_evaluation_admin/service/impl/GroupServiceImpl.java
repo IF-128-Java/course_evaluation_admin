fe48495f2b2e4f4ffc2a1512de7009f5d613a815
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-03 16:48:52 +0300

    fixed GroupServiceImpl

--- src/main/java/ita/softserve/course_evaluation_admin/service/impl/GroupServiceImpl.java
+++ src/main/java/ita/softserve/course_evaluation_admin/service/impl/GroupServiceImpl.java
@@ -6,20 +6,18 @@ import ita.softserve.course_evaluation_admin.entity.User;
 import ita.softserve.course_evaluation_admin.exception.exceptions.WrongIdException;
 import ita.softserve.course_evaluation_admin.repository.GroupRepository;
 import ita.softserve.course_evaluation_admin.service.GroupService;
-import ita.softserve.course_evaluation_admin.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class GroupServiceImpl implements GroupService {
     private final GroupRepository groupRepository;
-    private final UserService userService;
 
-    public GroupServiceImpl(GroupRepository groupRepository, UserService userService) {
+    public GroupServiceImpl(GroupRepository groupRepository) {
         this.groupRepository = groupRepository;
-        this.userService = userService;
     }
 
     @Override
@@ -35,6 +33,11 @@ public class GroupServiceImpl implements GroupService {
 
     @Override
     public Group addStudents(Group group, List<User> students) {
+        final Optional<User> notExpectedUser = students.stream()
+                .filter(s -> s.getGroup() != group && s.getGroup() != null).findFirst();
+        notExpectedUser.ifPresent(u -> {
+            throw new WrongIdException("The user with id: " + u.getId() + " already included in the group with id: " + u.getGroup().getId());
+        });
         List<User> studentList = students
                 .stream()
                 .peek(u -> u.setGroup(group)).collect(Collectors.toList());
@@ -44,6 +47,11 @@ public class GroupServiceImpl implements GroupService {
 
     @Override
     public Group removeStudents(Group group, List<User> students) {
+        final Optional<User> notExpectedUser = students.stream()
+                .filter(s -> s.getGroup() == null || !s.getGroup().equals(group)).findFirst();
+        notExpectedUser.ifPresent(u -> {
+            throw new WrongIdException("The user with id: " + u.getId() + " isn't included in the group");
+        });
         List<User> studentList = students
                 .stream()
                 .peek(u -> u.setGroup(null)).collect(Collectors.toList());
