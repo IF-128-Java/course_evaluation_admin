2865173d0676edd5fb4c4b549a8626aa556e27ce
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-09 21:19:46 +0300

    added repo tests

---
+++ src/test/java/ita/softserve/course_evaluation_admin/repository/CourseRepositoryTest.java
@@ -0,0 +1,78 @@
+package ita.softserve.course_evaluation_admin.repository;
+
+import ita.softserve.course_evaluation_admin.entity.Course;
+import ita.softserve.course_evaluation_admin.entity.Group;
+import ita.softserve.course_evaluation_admin.entity.Role;
+import ita.softserve.course_evaluation_admin.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DataJpaTest
+class CourseRepositoryTest {
+    private User teacher;
+    private Group group;
+    private Course course;
+
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @BeforeAll
+    public void beforeAll() {
+        teacher = User.builder()
+                .firstName("Mike")
+                .lastName("Wood")
+                .email("mike@com")
+                .roles(Set.of(Role.ROLE_TEACHER))
+                .password("password")
+                .build();
+        group = Group.builder()
+                .groupName("TestGroup")
+                .build();
+
+        course = Course.builder()
+                .courseName("TestCourse")
+                .description("Description")
+                .startDate(LocalDateTime.now())
+                .endDate(LocalDateTime.now().plusDays(2L))
+                .groups(Set.of(group))
+                .teacher(teacher)
+                .build();
+
+    }
+
+
+    @Test
+    void findAllByGroupId() {
+        userRepository.save(teacher);
+        groupRepository.save(group);
+        courseRepository.save(course);
+        group.setCourses(Set.of(course));
+
+        List<Course> actual = courseRepository.findAllByGroupId(group.getId());
+
+        assertTrue(actual.contains(course));
+        assertEquals(1,actual.size());
+    }
+
+    @Test
+    void findAllByNotExistingGroupId() {
+        final List<Course> actual = courseRepository.findAllByGroupId(1L);
+        assertTrue(actual.isEmpty());
+    }
+}
