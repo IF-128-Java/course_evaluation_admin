44f229ab6d70d359e56116deb4322029ecbd8b6e
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-11 14:26:32 +0300

    removed unused imports

--- src/main/java/ita/softserve/course_evaluation_admin/controller/CourseController.java
+++ src/main/java/ita/softserve/course_evaluation_admin/controller/CourseController.java
@@ -5,7 +5,6 @@ import ita.softserve.course_evaluation_admin.dto.mapper.CourseDtoMapper;
 import ita.softserve.course_evaluation_admin.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/ita/softserve/course_evaluation_admin/dto/CourseDto.java
+++ src/main/java/ita/softserve/course_evaluation_admin/dto/CourseDto.java
@@ -6,8 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
