cd8cf69e0c255986f6466a9f89bc60360028c199
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-11 14:12:39 +0300

    added group and course validations

--- src/main/java/ita/softserve/course_evaluation_admin/controller/CourseController.java
+++ src/main/java/ita/softserve/course_evaluation_admin/controller/CourseController.java
@@ -5,6 +5,7 @@ import ita.softserve.course_evaluation_admin.dto.mapper.CourseDtoMapper;
 import ita.softserve.course_evaluation_admin.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -36,7 +38,8 @@ public class CourseController {
     }
 
     @PostMapping
-    public ResponseEntity<CourseDto> createCourse(@RequestBody CourseDto dto) {
+    public ResponseEntity<CourseDto> createCourse(@Valid @RequestBody  CourseDto dto) {
+
         return ResponseEntity.status(HttpStatus.CREATED).
                 body(CourseDtoMapper.toDto(courseService.create(dto)));
     }
--- src/main/java/ita/softserve/course_evaluation_admin/dto/CourseDto.java
+++ src/main/java/ita/softserve/course_evaluation_admin/dto/CourseDto.java
@@ -1,19 +1,26 @@
 package ita.softserve.course_evaluation_admin.dto;
 
+import ita.softserve.course_evaluation_admin.annotation.StartDateBeforeEndDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@StartDateBeforeEndDate
 public class CourseDto {
     private Long id;
+    @Size(min = 3, message = "Course name should not be less than 3 !")
     private String courseName;
+    @Size(min = 10, message = "Course description should not be less than 10 !")
     private String description;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
