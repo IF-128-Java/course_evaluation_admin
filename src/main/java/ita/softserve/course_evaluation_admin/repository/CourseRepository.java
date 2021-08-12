db97bf8b5ab92ad3293cd0559de5127a9f9cd5fd
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-12 15:47:00 +0300

    added pagination

--- src/main/java/ita/softserve/course_evaluation_admin/repository/CourseRepository.java
+++ src/main/java/ita/softserve/course_evaluation_admin/repository/CourseRepository.java
@@ -1,6 +1,8 @@
 package ita.softserve.course_evaluation_admin.repository;
 
 import ita.softserve.course_evaluation_admin.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,6 +17,10 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
             "LEFT JOIN course_group cg ON c.id = cg.course_id  INNER JOIN users u ON u.id = c.teacher_id " +
             "WHERE (cg.group_id != ?1 OR cg.group_id IS NULL) and (concat(c.course_name,' ', u.first_name,' ', u.last_name) like concat('%',?2,'%')) AND c.id NOT IN " +
             "(SELECT course_id AS id FROM course_group WHERE group_id = ?1)"
-             , nativeQuery = true)
-    List<Course> findAllByFilterAndExcludeGroup(long excludeGroupId, String filter);
+             ,countQuery = "SELECT COUNT(DISTINCT c.id) FROM course c " +
+            "LEFT JOIN course_group cg ON c.id = cg.course_id  INNER JOIN users u ON u.id = c.teacher_id " +
+            "WHERE (cg.group_id != ?1 OR cg.group_id IS NULL) and (concat(c.course_name,' ', u.first_name,' ', u.last_name) like concat('%',?2,'%')) AND c.id NOT IN " +
+            "(SELECT course_id AS id FROM course_group WHERE group_id = ?1)"
+            , nativeQuery = true)
+    Page<Course> findAllByFilterAndExcludeGroup(long excludeGroupId, String filter, Pageable pageable);
 }
