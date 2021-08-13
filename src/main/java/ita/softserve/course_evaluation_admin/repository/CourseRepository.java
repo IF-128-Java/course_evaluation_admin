78f7025b2471b61d2c6d823407bc28646e850d83
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-13 15:20:54 +0300

    removed alias from queries

--- src/main/java/ita/softserve/course_evaluation_admin/repository/CourseRepository.java
+++ src/main/java/ita/softserve/course_evaluation_admin/repository/CourseRepository.java
@@ -9,11 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    @Query(value = "SELECT c.id AS id, c.course_name AS course_name, c.description AS description, c.start_date AS start_date, c.end_date AS end_date, " +
-            "c.teacher_id AS teacher_id FROM course c INNER JOIN course_group cg ON c.id = cg.course_id WHERE cg.group_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT c.id, c.course_name, c.description, c.start_date, c.end_date, " +
+            "c.teacher_id FROM course c INNER JOIN course_group cg ON c.id = cg.course_id WHERE cg.group_id = ?1", nativeQuery = true)
     List<Course> findAllByGroupId(long id);
 
-    @Query(value = "SELECT DISTINCT c.id AS id, c.course_name AS course_name, c.description AS description, c.start_date AS start_date, c.end_date AS end_date, c.teacher_id AS teacher_id FROM course c " +
+    @Query(value = "SELECT DISTINCT c.id, c.course_name, c.description, c.start_date, c.end_date, c.teacher_id FROM course c " +
             "LEFT JOIN course_group cg ON c.id = cg.course_id  INNER JOIN users u ON u.id = c.teacher_id " +
             "WHERE (cg.group_id != ?1 OR cg.group_id IS NULL) and (concat(c.course_name,' ', u.first_name,' ', u.last_name) like concat('%',?2,'%')) AND c.id NOT IN " +
             "(SELECT course_id AS id FROM course_group WHERE group_id = ?1)"
