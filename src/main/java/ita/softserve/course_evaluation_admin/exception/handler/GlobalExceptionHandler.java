22dac5d1b2b71440e54bafb2a9ed0e7ed9d37f2e
Vova <51489244+VovaProtsiv@users.noreply.github.com>
2021-08-26 22:24:53 +0300

    created UserRoleException

--- src/main/java/ita/softserve/course_evaluation_admin/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/ita/softserve/course_evaluation_admin/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package ita.softserve.course_evaluation_admin.exception.handler;
 import ita.softserve.course_evaluation_admin.exception.dto.GenericExceptionResponse;
 import ita.softserve.course_evaluation_admin.exception.exceptions.GroupAlreadyExistException;
 import ita.softserve.course_evaluation_admin.exception.exceptions.NotEmptyGroupException;
+import ita.softserve.course_evaluation_admin.exception.exceptions.UserRoleException;
 import ita.softserve.course_evaluation_admin.exception.exceptions.WrongIdException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
@@ -24,14 +25,14 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({WrongIdException.class, GroupAlreadyExistException.class})
+    @ExceptionHandler({WrongIdException.class,
+            GroupAlreadyExistException.class,
+            UserRoleException.class})
     public ResponseEntity<GenericExceptionResponse> handleWrongIdException(RuntimeException exception) {
         GenericExceptionResponse dto = GenericExceptionResponse.builder()
                 .message(exception.getMessage())
-                .status(HttpStatus.BAD_REQUEST.value())
                 .error(exception.getClass().getSimpleName())
                 .build();
-
         return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
     }
 
