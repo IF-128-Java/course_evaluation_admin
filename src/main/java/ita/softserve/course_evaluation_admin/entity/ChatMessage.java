18270ab6e74fe2144943bbbeaf14b44ff33736b3
Arsen-Kushnir <arsikkushnir@gmail.com>
2021-08-29 00:20:22 +0300

    When delete group then delete chat room and when delete messages!

---
+++ src/main/java/ita/softserve/course_evaluation_admin/entity/ChatMessage.java
@@ -0,0 +1,30 @@
+package ita.softserve.course_evaluation_admin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "chat_messages")
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+}
--- src/main/java/ita/softserve/course_evaluation_admin/entity/ChatRoom.java
+++ src/main/java/ita/softserve/course_evaluation_admin/entity/ChatRoom.java
@@ -3,7 +3,11 @@ package ita.softserve.course_evaluation_admin.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -11,11 +15,15 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@EqualsAndHashCode(exclude = {"messages"})
+@ToString(exclude = {"messages"})
 @Builder
 @Entity
 @Table(name = "chat_rooms")
@@ -28,4 +36,7 @@ public class ChatRoom {
     @Enumerated(EnumType.STRING)
     @Column(name = "chat_type", nullable = false)
     private ChatType chatType;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.REMOVE)
+    private List<ChatMessage> messages;
 }
